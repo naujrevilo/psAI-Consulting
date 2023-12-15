ab2db9faab6b670faddd1f1211a5593739ffffd1
Dex REAU <dev@oqva.digital>
2023-12-15 15:12:28 -0300

    copy update

--- consts.ts
+++ consts.ts
@@ -3,9 +3,9 @@
 
 export const SITE_TITLE = "psAI Consulting - Unlock the Potential of AI Integration";
 
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION = "AI integration through a blend of agility, insight, and innovation. Navigate tomorrow's complexities, transforming uncertainties into identifiable opportunities.";
 
-export const SITE_KEYWORDS = ""
+export const SITE_KEYWORDS = "artificial intelligence, AI Integration, AI consulting, AI development, business consulting, startup"
     
 
 // SOCIAL SHARING IMAGE
