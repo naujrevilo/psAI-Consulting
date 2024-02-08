887c3ac2291d35a60b2ae139ebb1bde0f702e456
Dex REAU <dev@oqva.digital>
2024-02-08 15:27:15 -0300

    up

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -18,13 +18,17 @@ gsap.registerPlugin(ScrollTrigger);
 // anim01.from(".default1 .list li", {x: 1300, opacity:0, duration: 1, ease: "power1.out",})
 
 const graphics = document.querySelectorAll('.graphic, .logo')
-const serviceLogos = document.querySelectorAll('.subnav svg')
+const serviceLogos = document.querySelectorAll('svg.service_logo')
+
+
+const serviceLogosOutHeader = document.querySelectorAll('main svg.service_logo')
 
 const relativeStrokeWidth = Math.floor((window.innerHeight + (window.innerWidth / 5)) / 580)
 
 
 for(let j = 0; j < graphics.length; j++) {
     let pulsePaths = graphics[j].querySelectorAll('.pulse')
+
     for(let i = 0; i < pulsePaths.length; i++) {
         pulsePaths[i].style.animationDelay = `${i / 15}s`
         pulsePaths[i].style.strokeWidth = `${relativeStrokeWidth}px`
@@ -33,13 +37,23 @@ for(let j = 0; j < graphics.length; j++) {
 
 for(let j = 0; j < serviceLogos.length; j++) {
     let pulsePaths = serviceLogos[j].querySelectorAll('.pulse')
+
     for(let i = 0; i < pulsePaths.length; i++) {
         pulsePaths[i].style.animationDelay = `${i / 15}s`
     }
 }
 
-const floatPaths = document.querySelectorAll('.float')
+for(let k = 0; k < serviceLogosOutHeader.length; k++) {
+    let serviceLogosOutHeaderPaths = serviceLogosOutHeader[k].querySelectorAll('.pulse')
 
-for(let i = 0; i < floatPaths.length; i++) {
-    floatPaths[i].style.animationDelay = `${i * (Math.random() + 1 * 2 )}s`
+    for(let i = 0; i < serviceLogosOutHeaderPaths.length; i++) {
+        serviceLogosOutHeaderPaths[i].style.animationDelay = `${i / 15}s`
+        serviceLogosOutHeaderPaths[i].style.strokeWidth = `${relativeStrokeWidth}px`
+    }
 }
+
+// const floatPaths = document.querySelectorAll('.float')
+
+// for(let i = 0; i < floatPaths.length; i++) {
+//     floatPaths[i].style.animationDelay = `${i * (Math.random() + 1 * 2 )}s`
+// }
