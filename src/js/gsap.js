37796d0fdefa50ccf597bbada81a0d98e1b98851
Dex REAU <dev@oqva.digital>
2024-02-08 08:32:53 -0300

    psAI pre-update

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -18,6 +18,7 @@ gsap.registerPlugin(ScrollTrigger);
 // anim01.from(".default1 .list li", {x: 1300, opacity:0, duration: 1, ease: "power1.out",})
 
 const graphics = document.querySelectorAll('.graphic, .logo')
+const serviceLogos = document.querySelectorAll('.subnav svg')
 
 const relativeStrokeWidth = Math.floor((window.innerHeight + (window.innerWidth / 5)) / 580)
 
@@ -27,6 +28,13 @@ for(let j = 0; j < graphics.length; j++) {
     for(let i = 0; i < pulsePaths.length; i++) {
         pulsePaths[i].style.animationDelay = `${i / 15}s`
         pulsePaths[i].style.strokeWidth = `${relativeStrokeWidth}px`
+    }
+}
+
+for(let j = 0; j < serviceLogos.length; j++) {
+    let pulsePaths = serviceLogos[j].querySelectorAll('.pulse')
+    for(let i = 0; i < pulsePaths.length; i++) {
+        pulsePaths[i].style.animationDelay = `${i / 15}s`
     }
 }
 
---
+++ src/js/header.js
@@ -0,0 +1,71 @@
+
+const header = document.querySelector('header');
+const hamBt = document.querySelector(".hambt");
+const nav = document.querySelector(".header_nav");
+const navBts = document.querySelectorAll(".header_nav a, .header_nav button");
+const exitArea = document.querySelector('.exit_area');
+const contactBox = document.querySelector('section.contact');
+const contactBts = document.querySelectorAll('.contact_bt');
+const pulsePaths = document.querySelectorAll('.pulse');
+
+let showingNav = false;
+let showingContactBox = false;
+
+function toggleExitArea() {
+    exitArea.classList.toggle('show', showingContactBox || showingNav);
+}
+
+function toggleNav() {
+    showingNav = !showingNav;
+    hamBt.classList.toggle('active', showingNav);
+    nav.classList.toggle("show", showingNav);
+    toggleExitArea();
+    
+    controlPathAnimation(showingNav ? 'paused' : 'running');
+
+    if (showingContactBox) {
+        toggleContactBox();
+    }
+}
+
+function controlPathAnimation(state) {
+    pulsePaths.forEach(path => path.style.animationPlayState = state);
+}
+
+function toggleContactBox() {
+    showingContactBox = !showingContactBox;
+    contactBox.classList.toggle('show', showingContactBox);
+    controlPathAnimation(showingContactBox ? 'paused' : 'running');
+    toggleExitArea();
+}
+
+hamBt.addEventListener("click", toggleNav);
+
+if (window.matchMedia('(max-width:850px)').matches) {
+    navBts.forEach(btn => {
+        btn.addEventListener("click", () => {
+            toggleNav();
+            if (showingContactBox) {
+                toggleContactBox();
+            }
+        });
+    });
+}
+
+window.addEventListener("scroll", () => {
+    if (showingNav) {
+        toggleNav();
+    }
+});
+
+contactBts.forEach(btn => {
+    btn.addEventListener('click', toggleContactBox);
+});
+
+exitArea.addEventListener('click', () => {
+    if (showingContactBox) {
+        toggleContactBox();
+    } else if (showingNav) {
+        toggleNav();
+    }
+});
