6319896a8fa3d8465e673df63cee0a7531454bfc
Dex REAU <dev@oqva.digital>
2024-03-11 12:22:46 -0300

    Reports Update

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,7 +23,22 @@ const podcastCollection = defineCollection({
 	}),
 });
 
+const reportsCollection = defineCollection({
+	schema: z.object({
+        documentTitle: z.string().optional(),
+		metaDescription: z.string().optional(),
+		heading: z.string(),
+		description: z.string(),
+		heroImage: z.string().optional(),
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+	}),
+});
+
 export const collections = {
     // 'blog': blogCollection,
-    'podcast': podcastCollection
+    'podcast': podcastCollection,
+	'reports': reportsCollection
 };
